95950294240bd6616d9795e79ed98f7ed065c8ea
JenniH-S95 <88773173+JenniH-S95@users.noreply.github.com>
2021-08-18 08:12:31 +0000

    20210818

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Täällä ollaan!");
+
+let ika = 26; // numerot kirjoitetaan ilman lainausmerkkejä
+ika = ika + 1;
+
+let ikakirjaimina = "26";
+
+console.log(ikakirjaimina);
+ikakirjaimina = ikakirjaimina + 1;
+
+console.log("Sinun ikäsi on: " + ika);
+
+/*
+Monen
+rivin
+kommentti
+*/
