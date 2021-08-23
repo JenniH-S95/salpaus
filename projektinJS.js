6b97f4aa3487ecc89f220343d60abab00015f942
JenniH-S95 <88773173+JenniH-S95@users.noreply.github.com>
2021-08-23 09:03:10 +0000

    20210823 projektissa checkboxit toimii

--- projektinJS.js
+++ projektinJS.js
@@ -7,4 +7,21 @@ document.querySelector('.nappi2').addEventListener('click', e =>{
   else{
     document.querySelector(".lootat").children[0].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="HTML">HTML</li>'
   }
-})
+
+  if(document.querySelector("#CSS").checked){document.querySelector(".lootat").children[1].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="CSS" checked>CSS kurssi suoritettu!</li>'}
+
+  else{
+    document.querySelector(".lootat").children[1].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="CSS">CSS</li>'
+  }
+  if(document.querySelector("#JavaScript").checked){document.querySelector(".lootat").children[2].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="JavaScript" checked>JavaScript kurssi suoritettu!</li>'}
+
+  else{
+    document.querySelector(".lootat").children[2].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="JavaScript">JavaScript</li>'
+  }
+
+  if(document.querySelector("#CICD").checked){document.querySelector(".lootat").children[3].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="CICD" checked>CI/CD kurssi suoritettu!</li>'}
+
+  else{
+    document.querySelector(".lootat").children[3].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="CICD">CI/CD</li>'
+  }
+});
