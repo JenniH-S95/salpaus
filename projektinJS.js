945f807a9a614ae15459e1d6b50b042ea40e67f3
JenniH-S95 <88773173+JenniH-S95@users.noreply.github.com>
2021-08-25 11:07:27 +0000

    20210825 projekti aika hyvässä vaiheessa, ainakin omasta mielestä.

--- projektinJS.js
+++ projektinJS.js
@@ -1,27 +1,43 @@
 document.querySelector('.nappi2').addEventListener('click', e =>{
   e.preventDefault();
-  console.log("nappi toimii!");
+  /*console.log("nappi toimii!");*/
 
-  if(document.querySelector("#HTML").checked){document.querySelector(".lootat").children[0].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="HTML" checked>HTML kurssi suoritettu!</li>'}
+  if(document.querySelector("#HTML").checked){
+    document.querySelector(".lootat").children[0].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="HTML" checked>HTML kurssi suoritettu!</li>'}
 
   else{
     document.querySelector(".lootat").children[0].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="HTML">HTML</li>'
   }
 
-  if(document.querySelector("#CSS").checked){document.querySelector(".lootat").children[1].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="CSS" checked>CSS kurssi suoritettu!</li>'}
+  if(document.querySelector("#CSS").checked){
+    document.querySelector(".lootat").children[1].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="CSS" checked>CSS kurssi suoritettu!</li>'}
 
   else{
     document.querySelector(".lootat").children[1].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="CSS">CSS</li>'
   }
-  if(document.querySelector("#JavaScript").checked){document.querySelector(".lootat").children[2].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="JavaScript" checked>JavaScript kurssi suoritettu!</li>'}
+  if(document.querySelector("#JavaScript").checked){
+    document.querySelector(".lootat").children[2].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="JavaScript" checked>JavaScript kurssi suoritettu!</li>'}
 
   else{
     document.querySelector(".lootat").children[2].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="JavaScript">JavaScript</li>'
   }
 
-  if(document.querySelector("#CICD").checked){document.querySelector(".lootat").children[3].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="CICD" checked>CI/CD kurssi suoritettu!</li>'}
+  if(document.querySelector("#CICD").checked){
+    document.querySelector(".lootat").children[3].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="CICD" checked>CI/CD kurssi suoritettu!</li>'}
 
   else{
     document.querySelector(".lootat").children[3].innerHTML = '<li class="ohjelmat"><input type="checkbox" id="CICD">CI/CD</li>'
   }
+});
+
+document.querySelector(".nappula1").addEventListener('click', e =>{
+  e.preventDefault();
+
+  /*document.querySelector('.tietolaatikko').innerHTML = document.querySelector('#nimi').value+": "+document.querySelector('#tieto').value;*/
+
+  if(document.querySelector('#nimi').value =="" || document.querySelector('#tieto').value ==""){
+    document.querySelector(".varoitus").innerHTML = "Täytä kaikki kentät!";
+    setTimeout(() => document.querySelector('.varoitus').innerHTML='', 3000);
+  }
+  else(document.querySelector('.tietolaatikko').innerHTML = document.querySelector('#nimi').value+": "+document.querySelector('#tieto').value);
 });
